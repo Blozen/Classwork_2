64451cef2391101cc6d56bea49e1e9189702e9af
Blozen <tm.eskov@gmail.com>
2021-12-10 22:24:50 +0300

    кружки

---
+++ main2.py
@@ -0,0 +1,42 @@
+import sys
+from PyQt5.QtGui import QPainter, QColor
+from PyQt5.QtWidgets import QMainWindow, QApplication
+from PyQt5 import uic
+import random
+
+
+class Progr(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('ui1.ui', self)
+        self.start()
+
+    def start(self):
+        self.do_paint = False
+        self.pushButton.clicked.connect(self.run)
+
+    def paintEvent(self, event):
+        if self.do_paint:
+            a = random.randrange(100)
+            qp = QPainter()
+            qp.begin(self)
+            qp.setBrush(QColor(255, 255, 0))
+            qp.drawEllipse(random.randrange(75, 400 - a), random.randrange(75, 500 - a), a, a)
+            qp.end()
+
+    def run(self):
+        self.do_paint = True
+        print(self.do_paint)
+        self.repaint()
+
+
+def except_hook(cls, exception, traceback):
+    sys.__excepthook__(cls, exception, traceback)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Progr()
+    ex.show()
+    sys.excepthook = except_hook
+    sys.exit(app.exec())
